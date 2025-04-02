0ab38c85543cbbff558e123d860edd88ac6d1774
Matheus Filipe Amorim <mahteus.amorim02@gmail.com>
2025-04-02 13:31:02 -0300

    initial Spring Boot project files

---
+++ src/main/java/com/github/mfilipeamorim/bookmanager_api/BookmanagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.mfilipeamorim.bookmanager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmanagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmanagerApiApplication.class, args);
+	}
+
+}
